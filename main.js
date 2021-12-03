8e8d5e9fb7503a0b146f6171add39cd84808c884
Brian Alexis Schulz <brian-alexix@hotmail.com>
2021-12-02 21:16:09 -0300

    JsCambios

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('El infierno chaqueño')
+  .typeString('El clima Chaqueño')
   .pauseFor(200)
   .deleteChars(10)
   .start();
